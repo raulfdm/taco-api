3d5c66acf1fbd38d7cc19e8ef1e8fdde6053abe4
Raul Melo <raulmelo@Rauls-MacBook-Pro.local>
2018-10-14 21:28:24 +0200

    Remove old API files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-const app = require('./config/express.config.js')();
-
-const port = process.env.PORT || 8080;
-
-app.listen(port, () => console.log(`Servidor Rodando na porta ${port}`));
+console.log('hi');
