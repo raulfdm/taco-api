c0ce16b4522c2681871a7f97cc4f31fbe4d125ef
Raul Melo <raulmelo@Rauls-MacBook-Pro.local>
2018-10-14 22:21:51 +0200

    Implement basic index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('hi');
+import foodList from './data/foodList.json';
+
+class TACO {
+  getAllFood() {
+    return foodList;
+  }
+
+  getFoodById(id) {
+    return foodList.filter(food => food.id === id);
+  }
+
+  getFoodByCategoryId(categoryId) {
+    return foodList.filter(food => food.category_id === categoryId);
+  }
+
+  getFoodByDescription(description) {
+    return foodList.filter(food =>
+      new RegExp(description, 'gi').test(food.description)
+    );
+  }
+}
+
+export default new TACO();
