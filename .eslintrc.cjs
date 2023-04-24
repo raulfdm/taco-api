bdd7dfc71139bb8844771702504f037617b8c651
Raul Melo <melo.raulf@gmail.com>
2023-04-24 16:13:07 +0200

    add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "simple-import-sort"],
+  root: true,
+  rules: {
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        varsIgnorePattern: "^_",
+        argsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+        ignoreRestSiblings: true,
+      },
+    ],
+  },
+};
