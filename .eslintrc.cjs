5f8a9b8ec5d228033eba5207c84e3fde19ebe4f7
Raul Melo <melo.raulf@gmail.com>
2023-04-25 09:09:17 +0200

    add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,29 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "simple-import-sort"],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'simple-import-sort',
+    'eslint-plugin-prettier',
+  ],
   root: true,
   rules: {
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
-    "@typescript-eslint/consistent-type-imports": "error",
-    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
-    "@typescript-eslint/no-unused-vars": [
-      "error",
+    'prettier/prettier': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
       {
-        varsIgnorePattern: "^_",
-        argsIgnorePattern: "^_",
-        destructuredArrayIgnorePattern: "^_",
+        varsIgnorePattern: '^_',
+        argsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
         ignoreRestSiblings: true,
       },
     ],
