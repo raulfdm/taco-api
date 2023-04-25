0d2353205519d6d242d47c5d3e968f5484c63516
Raul Melo <melo.raulf@gmail.com>
2023-04-25 09:00:43 +0200

    enhance disable logs

--- src/infrastructure/primaClient.ts
+++ src/infrastructure/primaClient.ts
@@ -1,13 +1,26 @@
 import { PrismaClient } from "@prisma/client";
 
+import { env } from "./env";
+
 type GetPrismaOpts = {
   disableLogs?: boolean;
 };
 
-export function getPrismaClient({ disableLogs }: GetPrismaOpts): PrismaClient {
+export function getPrismaClient({
+  disableLogs,
+}: GetPrismaOpts = {}): PrismaClient {
+  let shouldLog = true;
+
+  if (disableLogs === true) {
+    shouldLog = false;
+  }
+
+  if (env.NODE_ENV === "production") {
+    shouldLog = false;
+  }
+
   const prisma = new PrismaClient({
-    // TODO: change this to development only
-    log: disableLogs ? undefined : ["error", "info", "query", "warn"],
+    log: shouldLog ? ["error", "info", "query", "warn"] : undefined,
   });
 
   return prisma;
