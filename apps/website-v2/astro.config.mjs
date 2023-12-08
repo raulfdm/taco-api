e1189fa3d4963ed856266868be5e141ee101f68c
Raul de Melo <melo.raulf@gmail.com>
2023-12-08 14:00:40 -0300

    changes

--- apps/website-v2/astro.config.mjs
+++ apps/website-v2/astro.config.mjs
@@ -1,27 +1,38 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "TACO API",
+      defaultLocale: "root",
+      locales: {
+        root: {
+          label: "Português",
+          lang: "pt-BR",
+        },
+        en: {
+          label: "English",
+        },
+      },
+      social: {
+        github: "https://github.com/raulfdm/taco-api",
+      },
+      sidebar: [],
+      // sidebar: [
+      //   {
+      //     label: "Guides",
+      //     items: [
+      //       // Each item here is one entry in the navigation menu.
+      //       { label: "Example Guide", link: "/guides/example/" },
+      //     ],
+      //   },
+      //   {
+      //     label: "Reference",
+      //     autogenerate: { directory: "reference" },
+      //   },
+      // ],
+    }),
+  ],
 });
