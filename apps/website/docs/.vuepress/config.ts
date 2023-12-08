4779b7dd7f957b941bb73e1df9da2073bf17bc87
Raul de Melo <melo.raulf@gmail.com>
2023-12-08 11:38:48 -0300

    add lint and lint:ci commands

--- apps/website/docs/.vuepress/config.ts
+++ apps/website/docs/.vuepress/config.ts
@@ -1,67 +1,67 @@
-import { defaultTheme, defineUserConfig } from 'vuepress';
+import { defaultTheme, defineUserConfig } from "vuepress";
 
 export default defineUserConfig({
   theme: defaultTheme({
-    repo: 'raulfdm/taco-api',
+    repo: "raulfdm/taco-api",
     locales: {
-      '/': {
+      "/": {
         navbar: [
           {
-            text: 'Versão',
+            text: "Versão",
             children: [
               {
-                text: 'Changelog',
-                link: 'https://github.com/raulfdm/taco-api/blob/main/CHANGELOG.md',
+                text: "Changelog",
+                link: "https://github.com/raulfdm/taco-api/blob/main/CHANGELOG.md",
               },
               {
-                text: 'v1.0.0',
-                link: 'https://6447d8ec2c2ccb00085151ca--taco-api.netlify.app/',
+                text: "v1.0.0",
+                link: "https://6447d8ec2c2ccb00085151ca--taco-api.netlify.app/",
               },
             ],
           },
         ],
-        selectLanguageName: 'Português',
-        toggleColorMode: 'Alternar modo escuro',
-        openInNewWindow: 'Abrir em nova aba',
-        backToHome: 'Voltar para página inicial',
-        tip: 'Dica',
-        warning: 'Atenção',
-        danger: 'Perigo',
-        contributorsText: 'Contribuidores',
-        lastUpdatedText: 'Atualizado pela última vez em',
-        editLinkText: 'Edite esta página',
-        selectLanguageText: 'Idiomas',
+        selectLanguageName: "Português",
+        toggleColorMode: "Alternar modo escuro",
+        openInNewWindow: "Abrir em nova aba",
+        backToHome: "Voltar para página inicial",
+        tip: "Dica",
+        warning: "Atenção",
+        danger: "Perigo",
+        contributorsText: "Contribuidores",
+        lastUpdatedText: "Atualizado pela última vez em",
+        editLinkText: "Edite esta página",
+        selectLanguageText: "Idiomas",
       },
-      '/en/': {
+      "/en/": {
         navbar: [
           {
-            text: 'Version',
+            text: "Version",
             children: [
               {
-                text: 'Changelog',
-                link: 'https://github.com/raulfdm/taco-api/blob/main/CHANGELOG.md',
+                text: "Changelog",
+                link: "https://github.com/raulfdm/taco-api/blob/main/CHANGELOG.md",
               },
               {
-                text: 'v1.0.0',
-                link: 'https://6447d8ec2c2ccb00085151ca--taco-api.netlify.app/',
+                text: "v1.0.0",
+                link: "https://6447d8ec2c2ccb00085151ca--taco-api.netlify.app/",
               },
             ],
           },
         ],
-        selectLanguageName: 'English',
+        selectLanguageName: "English",
       },
     },
   }),
   locales: {
-    '/': {
-      lang: 'pt-BR',
-      title: 'TACO API',
-      description: 'Tabela Brasileira de Composição de Alimentos',
+    "/": {
+      lang: "pt-BR",
+      title: "TACO API",
+      description: "Tabela Brasileira de Composição de Alimentos",
     },
-    '/en/': {
-      lang: 'en-US',
-      title: 'TACO API',
-      description: 'Brazilian Table of Food Composition',
+    "/en/": {
+      lang: "en-US",
+      title: "TACO API",
+      description: "Brazilian Table of Food Composition",
     },
   },
 });
