c759dbc916006bc8c6f086095e1421aae1f0d65e
Raul de Melo <melo.raulf@gmail.com>
2023-12-14 20:06:31 -0300

    delete build config

--- apps/taco/scripts/build.ts
+++
@@ -1,9 +0,0 @@
-import { dependencies } from "../package.json";
-
-await Bun.build({
-  entrypoints: ["./src/app.ts"],
-  outdir: "./dist",
-  external: Object.keys(dependencies),
-  target: "bun",
-  splitting: true,
-});
