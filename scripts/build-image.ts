63dce2ddd04a7165a061089a21adf73767cb927d
Raul Melo <melo.raulf@gmail.com>
2023-12-26 07:34:37 -0300

    fix docker deploy

--- scripts/build-image.ts
+++ scripts/build-image.ts
@@ -1,5 +1,7 @@
 import { version } from "../apps/api/package.json";
 
+const tagVersion = `raulfdm/taco-api:v${version}`;
+const latestVersion = "raulfdm/taco-api:latest";
 Bun.spawnSync(
   [
     "docker",
@@ -7,12 +9,19 @@ Bun.spawnSync(
     "-f",
     "./apps/api/Dockerfile",
     "-t",
-    `raulfdm/taco-api:v${version}`,
+    tagVersion,
     "-t",
-    "raulfdm/taco-api:latest",
+    latestVersion,
     ".",
   ],
   {
     stdio: ["inherit", "inherit", "inherit"],
   },
 );
+
+console.log("Build");
+
+Bun.spawnSync(["docker", "push", latestVersion]);
+Bun.spawnSync(["docker", "push", tagVersion]);
+
+console.log("Docker images pushed");
